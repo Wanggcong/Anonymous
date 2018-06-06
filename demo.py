5391a24820431457aeda423413119913dfbf6fdb
Wanggcong <1311328021@qq.com>
2018-06-06 15:29:40 +0800

    v3.1 elegant version, no select gallery

--- demo.py
+++ demo.py
@@ -4,9 +4,6 @@ import numpy as np
 import math
 import matplotlib.animation as animation 
 
-
-global_box_selected = []
-
 def on_press(event):
     if event.inaxes == None:
         print("none")
@@ -20,15 +17,14 @@ def on_press(event):
     dis = np.array(dis)
     # smallest distance
     min_ind = np.where(dis==np.min(dis))[0][0]
-    box_selected = top_points[9][min_ind]
+    top_selected = top_points[9][min_ind]
+    box_selected = [top_selected[0],top_selected[1],top_selected[0]+resize_h,top_selected[1]+resize_w]
     print('top_points:',top_points)
     print('box_selected:',box_selected)
 
-    global_box_selected = box_selected
-
-    # fig.canvas.mpl_disconnect(cid)
+    # select one candicate
     draw = ImageDraw.Draw(im)
-    draw.rectangle([box_selected[0], box_selected[1], box_selected[0]+resize_h, box_selected[1]+resize_w], outline=(0, 255, 0))
+    draw.rectangle(box_selected, outline=(0, 255, 0))
     print('min_ind:',min_ind)
     plt.imshow(im)
     fig.canvas.draw()
@@ -41,10 +37,7 @@ def on_press(event):
     for i in range(9):
         ch = top_points_rela_path[i]
         features_ch = features_dict[ch] 
-        
         dis_ch = np.sum((feat - features_ch)**2, axis=1)
-
-
         print('(feat - features_ch) shape:',(feat - features_ch).shape)
         print('dis_ch shape:',dis_ch.shape)
         print('dis_ch:',dis_ch)
@@ -63,16 +56,13 @@ def on_press(event):
             box = [top[0],top[1],top[0]+resize_h,top[1]+resize_w]
             im.paste(search_img, box)
             draw = ImageDraw.Draw(im)
-            draw.rectangle([box[0], box[1], box[0]+resize_h, box[1]+resize_w], outline=(255, 0, 0))              
+            draw.rectangle(box, outline=(255, 0, 0))              
     plt.imshow(im)
     fig.canvas.draw()
-
 
     # step 5: select which one  and compute which center, show green boxes    
 
     # reset
-
-
 
 im_path = '/home/wang/projects/super-computer/school2.JPG';
 im = Image.open(im_path)
@@ -83,22 +73,17 @@ print(im.size, width, height)
 print(im.format, im.format_description)
 
 # (w, h)
-crop_h = 40
-crop_w = 55
-
 row1_h = [200]
 row1_w = [600]
+# resize_h = 55
+# resize_w = 40
 
-cropedIm = im.crop((row1_h[0], row1_w[0], row1_h[0]+crop_h, row1_w[0] + crop_w))
-# cropedIm.show()
-
-
-resize_h = 55
-resize_w = 40
+resize_h = 38
+resize_w = 48
 
 
-thelta_h = 5
-thelta_w = 5
+thelta_h = 15
+thelta_w = 15
 
 
 row1_h0 = 180
@@ -106,8 +91,6 @@ row1_w0 = 73
 
 top_points_l = []
 for k in range(5):
-    # row1_h = []
-    # row1_w = []
     floor_k = []
     row1_w_k = row1_w0 + k*(thelta_h+resize_h)
     for i in range(5):
@@ -119,44 +102,28 @@ for k in range(5):
 
 ##########################################################
 # top_points
-#
-
-
 row1_h0 = 825
 row1_w0 = 73
 
 top_points_r = []
 for k in range(5):
-    # row1_h = []
-    # row1_w = []
     floor_k = []
     row1_w_k = row1_w0 + k*(thelta_h+resize_h)
-    for i in range(5):
-        # row1_h.append(row1_h0+i*(thelta_w+resize_w))
-        # row1_w.append(row1_w_k)    
+    for i in range(5): 
         floor_k.append([row1_h0+i*(thelta_w+resize_w),row1_w_k])
-    top_points_r.append(floor_k)
-    # for i in range(5):
-    #     im.paste(cropedIm, (row1_h[i], row1_w[i]))   
+    top_points_r.append(floor_k)  
 top_points = top_points_r + top_points_l
 
+features = np.load('demo-features/features/ch22/features.npy')
+fnames = np.load('demo-features/features/ch22/fnames.npy')
 
-features = np.load('demo-features/features/ch02/features.npy')
-fnames = np.load('demo-features/features/ch02/fnames.npy')
+top_points_l_rela_path = ['ch03','ch26','ch24','ch30','ch22']  # from floor 5 to floor 1, lift
+top_points_r_rela_path = ['ch27','ch16','ch08','ch02','ch18']  # from floor 5 to floor 1, east
 
-
-
-
-# top_points_l_rela_path = ['ch03','ch26','ch24','ch30','ch22']  # from floor 5 to floor 1, lift
-# top_points_r_rela_path = ['ch27','ch16','ch08','ch02','ch18']  # from floor 5 to floor 1, east
-
-top_points_l_rela_path = ['ch02','ch02','ch02','ch02','ch02']  # from floor 5 to floor 1, lift
-top_points_r_rela_path = ['ch02','ch02','ch02','ch02','ch02']  # from floor 5 to floor 1, east
+# top_points_l_rela_path = ['ch02','ch02','ch02','ch02','ch02']  # debug
+# top_points_r_rela_path = ['ch02','ch02','ch02','ch02','ch02']  # debug
 
 top_points_rela_path = top_points_r_rela_path + top_points_l_rela_path
-
-
-
 features_dict = {}  
 fnames_dict = {}  
 for i in range(9):
@@ -181,6 +148,7 @@ imgs_path = '/media/wang/mySATA/datasets/supercomputer_choose/PROI-Patch/'+ top_
 
 # step 1: show building
 
+
 # step 2: show paste floor 1
 random_indexes = np.random.choice(fnames.shape[0], size=5, replace=False)
 # im.close()
@@ -192,8 +160,10 @@ for i in range(5):
     box = [top[0],top[1],top[0]+resize_h,top[1]+resize_w]
     im.paste(target, box)
     draw = ImageDraw.Draw(im)
-    draw.rectangle([box[0], box[1], box[0]+resize_h, box[1]+resize_w], outline=(255, 0, 0))    
+    draw.rectangle(box, outline=(255, 0, 0))    
 fig = plt.figure()
+
+
 
 
 # step 3: select which one  and compute which center, show green boxes
